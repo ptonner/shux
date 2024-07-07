8f91ee9fc674f7da554a2666d77f2885aa433b9c
Peter Tonner <peter.tonner@proton.me>
2024-07-07 18:03:33 -0400

    refactor runtime dir code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::error::Error;
+use std::convert::Infallible;
 use std::process::Command;
+use std::{error::Error, str::FromStr};
 // use std::fs;
-// use std::path::PathBuf;
+use std::path::PathBuf;
 // use jupyter_client::Client;
 
 use clap::{Parser, Subcommand};
@@ -19,21 +20,27 @@ enum Commands {
     Show {},
 }
 
+fn jupyter_runtime_dir() -> Result<PathBuf, Infallible> {
+    let jupyter_runtime_cmd = Command::new("jupyter")
+        .args(["--runtime-dir"])
+        .output()
+        .expect("jupyter should be installed");
+    let jupyter_runtime_output = String::from_utf8_lossy(&jupyter_runtime_cmd.stdout);
+    PathBuf::from_str(
+        jupyter_runtime_output
+            .strip_suffix("\n")
+            .expect("jupyter should report runtime directory"),
+    )
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
-
     match &cli.command {
         Some(Commands::Show {}) => {
-            let jupyter_runtime_dir = Command::new("jupyter")
-                .args(["--runtime-dir"])
-                .output()
-                .expect("Jupyter not found");
-            println!(
-                "runtime: {:?}",
-                String::from_utf8(jupyter_runtime_dir.stdout)?
-                    .strip_suffix("\n")
-                    .unwrap()
-            );
+            // TODO: add test that this lists for different runtime dirs (specify from environment
+            // var)
+            let jupyter_runtime_dir = jupyter_runtime_dir();
+            println!("runtime: {:?}", jupyter_runtime_dir);
             // TODO: list connections at identified path
             // TODO: explore using `jupyter_client::find_connection_file`, patching broken linux
             // runtime spec
