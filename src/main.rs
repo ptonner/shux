6ae614a229a9c56a62128f0b992e6cbcb7f886fb
Peter Tonner <peter.tonner@proton.me>
2024-07-09 21:25:55 -0400

    start back on a native zmq connection

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,22 @@
-use std::convert::Infallible;
-use std::process::Command;
-use std::{error::Error, str::FromStr};
-// use std::fs;
-use std::path::PathBuf;
-// use jupyter_client::Client;
+use std::fs;
+use std::{error::Error, path::PathBuf};
 
-use clap::{Parser, Subcommand};
+use clap::Parser;
+
+pub mod paths;
+pub mod select;
 
 #[derive(Parser)]
 #[command(version, about, long_about=None)]
 struct Cli {
-    #[command(subcommand)]
-    command: Option<Commands>,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Show available kernel connections
-    Show {},
-}
-
-fn jupyter_runtime_dir() -> Result<PathBuf, Infallible> {
-    let jupyter_runtime_cmd = Command::new("jupyter")
-        .args(["--runtime-dir"])
-        .output()
-        .expect("jupyter should be installed");
-    let jupyter_runtime_output = String::from_utf8_lossy(&jupyter_runtime_cmd.stdout);
-    PathBuf::from_str(
-        jupyter_runtime_output
-            .strip_suffix("\n")
-            .expect("jupyter should report runtime directory"),
-    )
+    connection_file: PathBuf,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
-    match &cli.command {
-        Some(Commands::Show {}) => {
-            // TODO: add test that this lists for different runtime dirs (specify from environment
-            // var)
-            let jupyter_runtime_dir = jupyter_runtime_dir();
-            println!("runtime: {:?}", jupyter_runtime_dir);
-            // TODO: list connections at identified path
-            // TODO: explore using `jupyter_client::find_connection_file`, patching broken linux
-            // runtime spec
-        }
-        None => {}
-    }
-
+    let data = fs::read(cli.connection_file)?;
+    let data = String::from_utf8_lossy(&data).to_owned();
+    let connection_cfg: serde_json::Value = serde_json::from_str(&data)?;
+    println!("{:}", connection_cfg);
     Ok(())
 }
