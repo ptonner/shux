d124ee431130ae5bdd590c612acc4ab1ed9360a3
Peter Tonner <peter.tonner@proton.me>
2024-11-12 19:56:41 -0500

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use serde::{Deserialize, Serialize};
 use std::fs;
+use std::time::Duration;
 use std::{error::Error, path::PathBuf};
+use tokio::time::timeout;
 // use tokio::{main, test};
-use zeromq::{Socket, SocketRecv, SocketSend};
+use zeromq::{ReqSocket, Socket, SocketRecv, SocketSend};
 
 use clap::Parser;
 
@@ -13,6 +15,14 @@ pub mod select;
 #[command(version, about, long_about=None)]
 struct Cli {
     connection_file: PathBuf,
+}
+
+enum JupyterSocket {
+    Control,
+    Shell,
+    Stdin,
+    HeartBeat,
+    IOPub,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -28,18 +38,56 @@ struct ConnectionFile {
     ip: String,
 }
 
+impl ConnectionFile {
+    fn address(&self, socket: JupyterSocket) -> String {
+        let base = format!("{}://{}", self.transport, self.ip);
+        match socket {
+            JupyterSocket::Control => format!("{}:{}", base, self.control_port),
+            JupyterSocket::Shell => format!("{}:{}", base, self.shell_port),
+            JupyterSocket::Stdin => format!("{}:{}", base, self.stdin_port),
+            JupyterSocket::HeartBeat => format!("{}:{}", base, self.hb_port),
+            JupyterSocket::IOPub => format!("{}:{}", base, self.iopub_port),
+        }
+    }
+}
+
 fn load_connection_file(path: PathBuf) -> Result<ConnectionFile, Box<dyn Error>> {
     let data = fs::read(path)?;
     let data = String::from_utf8_lossy(&data).to_owned();
     Ok(serde_json::from_str(&data)?)
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+async fn run_heartbeat(mut socket: ReqSocket) -> Result<(), Box<dyn Error>> {
+    loop {
+        if let Err(e) = timeout(Duration::from_secs(5), socket.send("hb".into())).await? {
+            break Err(Box::new(e));
+        }
+        let msg = match timeout(Duration::from_secs(5), socket.recv()).await? {
+            Ok(msg) => msg,
+            Err(e) => break Err(Box::new(e)),
+        };
+        dbg!(msg);
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
-    // let data = fs::read(cli.connection_file)?;
-    // let data = String::from_utf8_lossy(&data).to_owned();
     let connection_cfg = load_connection_file(cli.connection_file)?;
-    println!("{:?}", connection_cfg);
+    // println!("{:?}", connection_cfg.address(JupyterSocket::HeartBeat));
+    let mut socket = ReqSocket::new();
+    socket
+        .connect(&connection_cfg.address(JupyterSocket::HeartBeat))
+        .await
+        .expect("Heart beat should be available");
+    if let Err(e) = run_heartbeat(socket).await {
+        println!("{:}", e)
+    };
+    // for _ in 0..10u64 {
+    //     socket.send("Hello".into()).await?;
+    //     let repl = socket.recv().await?;
+    //     dbg!(repl);
+    // }
     Ok(())
 }
 
