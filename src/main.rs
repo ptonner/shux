30a7b50fbfa6e088a08458bcd2aafea120067966
Peter Tonner <peter.tonner@proton.me>
2024-11-16 09:14:36 -0500

    add watch, start tui

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use std::convert::Infallible;
-use std::process::Command;
-use std::{error::Error, str::FromStr};
-// use std::fs;
+use std::fs::File;
+use std::io;
 use std::path::PathBuf;
-// use jupyter_client::Client;
+use std::process::Command;
+use std::thread;
+use std::time::Duration;
+use std::{error::Error, str::FromStr};
 
 use clap::{Parser, Subcommand};
+use jupyter_client::Client;
+use ratatui::{
+    crossterm::event::{self, KeyCode, KeyEventKind},
+    style::Stylize,
+    widgets::Paragraph,
+    DefaultTerminal,
+};
 
 #[derive(Parser)]
 #[command(version, about, long_about=None)]
@@ -18,6 +27,10 @@ struct Cli {
 enum Commands {
     /// Show available kernel connections
     Show {},
+    Watch {
+        kernel: PathBuf,
+    },
+    UI {},
 }
 
 fn jupyter_runtime_dir() -> Result<PathBuf, Infallible> {
@@ -33,6 +46,23 @@ fn jupyter_runtime_dir() -> Result<PathBuf, Infallible> {
     )
 }
 
+fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
+    loop {
+        terminal.draw(|frame| {
+            let greeting = Paragraph::new("Hello Ratatui! (press 'q' to quit)")
+                .white()
+                .on_blue();
+            frame.render_widget(greeting, frame.area());
+        })?;
+
+        if let event::Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                return Ok(());
+            }
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
     match &cli.command {
@@ -44,6 +74,38 @@ fn main() -> Result<(), Box<dyn Error>> {
             // TODO: list connections at identified path
             // TODO: explore using `jupyter_client::find_connection_file`, patching broken linux
             // runtime spec
+        }
+        Some(Commands::UI {}) => {
+            let mut terminal = ratatui::init();
+            terminal.clear()?;
+            let app_result = run(terminal);
+            ratatui::restore();
+            println!("{:?}", app_result);
+        }
+        Some(Commands::Watch { kernel }) => {
+            println!("watching: {:?}", kernel);
+            match File::open(kernel) {
+                Ok(reader) => {
+                    match Client::from_reader(reader) {
+                        Ok(client) => {
+                            // iopub
+                            let receiver = client.iopub_subscribe()?;
+                            thread::spawn(move || {
+                                for msg in receiver {
+                                    println!("Received message from kernel: {:#?}", msg);
+                                }
+                            });
+
+                            // wait
+                            loop {
+                                thread::sleep(Duration::from_secs(2));
+                            }
+                        }
+                        Err(err) => println!("Client error: {:?}", err),
+                    }
+                }
+                Err(err) => println!("File error: {:?}", err),
+            }
         }
         None => {}
     }
